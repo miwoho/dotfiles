9d8d0dc8b19574e93d011dab159cac4b0ae7d901
inpudiy <inpudiy@gmail.com>
2025-11-10 14:35:35 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#pacstrap -K base base-devel linux linux-firmware amd-ucode sudo git dosfstools mtools efibootmgr grub networkmanager neovim man-db man-pages htop python usbutils
+
+install_pacman() {
+    sudo pacman -S linux-firmware-qlogic mesa vulkan-radeon udiskie udisks2 7zip \
+    python-pip go clang cmake eslint pyright rust-analyzer rustup jre-openjdk \
+    gtk3 gtk4 qt5-wayland qt6-wayland xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk brightnessctl \
+    polkit polkit-gnome ttf-dejavu ttf-nerd-fonts-symbols noto-fonts-cjk \
+    bluez bluez-utils pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse \
+    wayland hyprland ly wl-clipboard waybar wofi mako bluetui zsh ghostty zed \
+    firefox telegram-desktop kicad obs-studio openscad gimp inkscape libreoffice-fresh showtime kdenlive audacity qbittorrent android-tools
+
+}
+
+install_aur() {
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+}
+
+enable_service() {
+    systemctl --user enable pipewire.service
+    systemctl --user enable pipewire-pulse.service
+    systemctl --user enable wireplumber.service
+
+    sudo systemctl enable ly.service
+    sudo systemctl enable bluetooth.service
+}
+
+copy_config(){
+    cp -r config/. ~/.config
+    sudo cp -r flexoki /usr/share/themes/flexoki
+    cp  .zshrc ~/
+}
+
+gtk_setting() {
+    gsettings set org.gnome.desktop.interface gtk-theme flexoki
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+}
+
+install_pacman
+enable_service
+copy_config
+gtk_setting()
