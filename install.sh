d2767e4aebe1a974735daf185ae45257d1959cab
miwoho <miwayho@gmail.com>
2025-11-16 12:01:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #pacstrap -K base base-devel linux linux-firmware amd-ucode sudo git dosfstools mtools efibootmgr grub networkmanager neovim man-db man-pages htop python usbutils
 
 install_pacman() {
-    sudo pacman -S linux-firmware-qlogic mesa vulkan-radeon udiskie udisks2 7zip \
+    sudo pacman -S linux-firmware-qlogic mesa vulkan-radeon udiskie udisks2 zip unzip unrar \
     python-pip go clang cmake eslint pyright rust-analyzer rustup jre-openjdk \
-    gtk3 gtk4 qt5-wayland qt6-wayland xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk brightnessctl openssh\
-    polkit polkit-gnome ttf-dejavu ttf-nerd-fonts-symbols noto-fonts-cjk zip unzip unrar\
+    gtk3 gtk4 qt5-wayland qt6-wayland xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk brightnessctl openssh \
+    polkit polkit-gnome ttf-dejavu ttf-nerd-fonts-symbols noto-fonts-cjk \
     bluez bluez-utils pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse \
     wayland hyprland ly wl-clipboard waybar wofi mako hyprshot bluetui zsh ghostty zed \
-    firefox telegram-desktop kicad obs-studio gimp inkscape libreoffice-fresh showtime loupe kdenlive audacity qbittorrent android-tools obsidian
-
+    firefox telegram-desktop kicad freecad gimp inkscape kdenlive audacity libreoffice-fresh \
+    qbittorrent obs-studio showtime loupe obsidian android-tools
 }
 
 install_aur() {
